e78b93edebf32d45b050e003654a95fa6324574d
Chuks-project <chuksy237@yahoo.com>
2023-05-30 22:11:29 +0100

    changes

--- terraform/modules/VPC/roles.tf
+++ terraform/modules/VPC/roles.tf
@@ -47,9 +47,9 @@
 #   policy_arn = aws_iam_policy.policy.arn
 # }
 
-# resource "aws_iam_instance_profile" "ip" {
-#   name = "aws_instance_profile_test"
-#   role = aws_iam_role.ec2_instance_role.name
-# }
+resource "aws_iam_instance_profile" "ip" {
+  name = "aws_instance_profile_test"
+  role = aws_iam_role.ec2_instance_role.name
+}
 
 
