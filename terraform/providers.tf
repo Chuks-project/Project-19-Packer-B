8dc9d4997d7b8ec4951d73f85f37843f9d8b002e
Chuks-project <chuksy237@yahoo.com>
2023-05-30 11:14:58 +0100

    changes

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,3 +1,12 @@
-provider  "aws"  {
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.30.0"
+    }
+  }
+}
+
+provider "aws" {
   region = var.region
 }
